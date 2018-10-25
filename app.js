8b9959165e69aad7b099db1237d624673e0e2433
Mia Ćaćić <cacic.miaa@gmail.com>
2018-10-25 14:01:27 +0200

    MVO change, error in el

--- app.js
+++ app.js
@@ -1,71 +1,121 @@
-const container = document.querySelector('.container');
-const catList = document.createElement('ol');
-catList.setAttribute('class', 'catList');
-container.appendChild(catList);
-const seeMeow = document.createElement('div');
-seeMeow.setAttribute('class',"seeMeow");
 
+var model = {
+    currentCat: null,
+    cats: [
+       { name:'Cato', 
+        image:'img/cat_b.jpg',
+        click: 0,
+        score: `${this.name} was clicked: ${this.click} time(s)`,
+        el: 0
+       },
 
-class Cat {
-  constructor(name,image) {
-    this.name = name;
-    this.image = image;
-    this.click = 0;
-    this.score = `${this.name} was clicked: ${this.click} time(s)`;
-    this.reachImg = 0;
-    this.reachCat=0;
-    this.reachScore= 0;
+      { name:'Gato', 
+        image:'img/cat2.jpg',
+        click: 0,
+        score: `${this.name} was clicked: ${this.click} time(s)`,
+        el: 0
+        },
 
+        {name:'Mato', 
+        image:'https://r.hswstatic.com/w_907/gif/tesla-cat.jpg',
+        click: 0,
+        score: `${this.name} was clicked: ${this.click} time(s)`,
+        el: 0
+       },
+
+        {name:'Zlato', 
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4NUg90aJh3gM6zchd5OQMxDighSXpvg4xYY5kM1JnHY83cU2Z',
+        click: 0,
+        score: `${this.name} was clicked: ${this.click} time(s)`,
+        el: 0
+       },
+
+        {name:'Nato', 
+        image:'https://boygeniusreport.files.wordpress.com/2017/01/cat.jpg?quality=98&strip=all&w=782',
+        click: 0,
+        score: `${this.name} was clicked: ${this.click} time(s)`,
+        el: 0
+        }
+    ]
   }
-
-  makeHTML() {
-    let catName = document.createElement('li');
-    catName.setAttribute('class',`${this.name}`);
-    catName.innerHTML= `${this.name}`;
-    catList.appendChild(catName);
-   
-
-    this.reachCat= document.querySelector(`.${this.name}`);
-    this.reachCat.addEventListener('click', this.seeCat.bind(this));
-   
-    
+var octopus = {
+  init: function(){
+    model.currentCat= model.cats[0];
+    listView.init();
+    catView.init();
+  },
+  getCats: function(){
+    return model.cats;
+  },
+  getCurrent: function(){
+    return model.currentCat;
+  },
+  setCurrent: function(cat){
+    model.currentCat = cat;
+  },
+  incrementer: function(){
+    model.currentCat.click++;
+    catView.render();
   }
-  seeCat() {
-    console.log(this);
-    seeMeow.innerHTML= "";
-    
-    let score = document.createElement('p');
-    score.setAttribute('class',`${this.name}-score`);
-    score.innerHTML= this.score;
-    let pic = document.createElement('img');
-    pic.setAttribute('src', this.image);
-    pic.setAttribute('alt', this.name);
-    pic.setAttribute('class',`${this.name}-img`);
-    seeMeow.appendChild(score);
-    seeMeow.appendChild(pic);
-    container.appendChild(seeMeow);
-    this.reachImg = document.querySelector(`.${this.name}-img`);
-    this.reachScore= document.querySelector(`.${this.name}-score`);
-    this.reachImg.addEventListener('click', this.clickCount.bind(this));
-
-  }
-  clickCount() {
-
-    this.click++;
-    this.score = `${this.name} was clicked: ${this.click} time(s)`;
-    this.reachScore.textContent = this.score;
-  }
-
-
 }
 
-const cat1 = new Cat('Cato', 'img/cat_b.jpg');
-const cat2 = new Cat('Gato', 'img/cat2.jpg');
-const cat3 = new Cat('Mato', 'https://r.hswstatic.com/w_907/gif/tesla-cat.jpg');
-const cat4 = new Cat('Zlato', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4NUg90aJh3gM6zchd5OQMxDighSXpvg4xYY5kM1JnHY83cU2Z');
-const cat5 = new Cat('Nato', 'https://boygeniusreport.files.wordpress.com/2017/01/cat.jpg?quality=98&strip=all&w=782');
-const cats = [];
-cats.push(cat1, cat2, cat3, cat4, cat5);
-cats.forEach(cat => {
-  cat.makeHTML();
-});
+var listView = {
+
+  init:function(){
+
+    this.container = document.querySelector('.container');
+    this.catList = document.querySelector('.catList');
+   
+    this.render();
+  },
+  render: function(){
+    var cats = octopus.getCats();
+    this.catList.innerHTML="";
+    
+    for (var i=0; i<cats.length; i++ ) {
+      var cat = cats[i];
+       var el =document.createElement('li');
+      el.setAttribute('class', `${cat.name}-name`)
+      el.textContent = `${cat.name}`;
+    
+      el.addEventListener('click', (function(catCopy){
+        return function(){
+          octopus.setCurrent(catCopy);
+          catView.render();
+        }
+      })(cat));
+
+      this.Catlist.appendChild(el);
+    };
+  }
+
+}
+ var catView = {
+   init: function(){
+    this.seeMeow = document.querySelector(".seeMeow");
+    this.p = document.querySelector('.info');
+    this.img = document.querySelector('.cat-img');
+    this.img.addEventListener('click', function(){
+      return function(){
+        octopus.incrementer();
+      }
+    });
+    this.render();
+   },
+
+   render: function(){
+     var currentCat = octopus.getCurrent();
+    
+     this.p.innerHTML = currentCat.score;
+     this.img.setAttribute('src', currentCat.image);
+     this.img.setAttribute('alt',currentCat.name);
+    
+   
+
+
+
+   }
+
+ };
+
+octopus.init();
